blurrycontour/zuul-gerrit

Use ssh for git-upload-pack
Currently git-upload-pack is executed via an unauthenticated https
call. This doesn't work if the project is not world
readable. Switching git-upload-pack to use ssh like all other commands
resolved this issue.

Change-Id: I26bc6c8937a6b8cdd2577f32c67b708211144fbd

tests/base.py
@@ -32,12 +32,10 @@ import subprocess
 import swiftclient
 import threading
 import time
-import urllib2
 
 import git
 import gear
 import fixtures
-import six.moves.urllib.parse as urlparse
 import statsd
 import testtools
 from git import GitCommandError
@@ -440,6 +438,18 @@ class FakeGerrit(object):
     def startWatching(self, *args, **kw):
         pass
 
+    def uploadPack(self, project):
+        ret = ('00a31270149696713ba7e06f1beb760f20d359c4abed HEAD\x00'
+               'multi_ack thin-pack side-band side-band-64k ofs-delta '
+               'shallow no-progress include-tag multi_ack_detailed no-done\n')
+        path = os.path.join(self.upstream_root, project)
+        repo = git.Repo(path)
+        for ref in repo.refs:
+            r = ref.object.hexsha + ' ' + ref.path + '\n'
+            ret += '%04x%s' % (len(r) + 4, r)
+        ret += '0000'
+        return ret
+
 
 class BuildHistory(object):
     def __init__(self, **kw):
@@ -448,29 +458,6 @@ class BuildHistory(object):
     def __repr__(self):
         return ("<Completed build, result: %s name: %s #%s changes: %s>" %
                 (self.result, self.name, self.number, self.changes))
-
-
-class FakeURLOpener(object):
-    def __init__(self, upstream_root, fake_gerrit, url):
-        self.upstream_root = upstream_root
-        self.fake_gerrit = fake_gerrit
-        self.url = url
-
-    def read(self):
-        res = urlparse.urlparse(self.url)
-        path = res.path
-        project = '/'.join(path.split('/')[2:-2])
-        ret = '001e# service=git-upload-pack\n'
-        ret += ('000000a31270149696713ba7e06f1beb760f20d359c4abed HEAD\x00'
-                'multi_ack thin-pack side-band side-band-64k ofs-delta '
-                'shallow no-progress include-tag multi_ack_detailed no-done\n')
-        path = os.path.join(self.upstream_root, project)
-        repo = git.Repo(path)
-        for ref in repo.refs:
-            r = ref.object.hexsha + ' ' + ref.path + '\n'
-            ret += '%04x%s' % (len(r) + 4, r)
-        ret += '0000'
-        return ret
 
 
 class FakeGerritTrigger(zuul.trigger.gerrit.Gerrit):
@@ -918,15 +905,6 @@ class ZuulTestCase(BaseTestCase):
         self.useFixture(fixtures.MonkeyPatch('swiftclient.client.Connection',
                                              FakeSwiftClientConnection))
         self.swift = zuul.lib.swift.Swift(self.config)
-
-        def URLOpenerFactory(*args, **kw):
-            if isinstance(args[0], urllib2.Request):
-                return old_urlopen(*args, **kw)
-            args = [self.fake_gerrit] + list(args)
-            return FakeURLOpener(self.upstream_root, *args, **kw)
-
-        old_urlopen = urllib2.urlopen
-        urllib2.urlopen = URLOpenerFactory
 
         self.launcher = zuul.launcher.gearman.Gearman(self.config, self.sched,
                                                       self.swift)

rabadin/zuul

resolves this issue.
Cherry-picked from https://review.openstack.org/#/c/200034/4
Partial JIRA: GALACTIC-1261

tests/base.py
@@ -22,7 +22,6 @@ import logging
 import os
 import pprint
 from six.moves import queue as Queue
-from six.moves import urllib
 import random
 import re
 import select
@@ -483,6 +482,18 @@ class FakeGerritConnection(zuul.connection.gerrit.GerritConnection):
     def _start_watcher_thread(self, *args, **kw):
         pass
 
+    def _uploadPack(self, project):
+        ret = ('00a31270149696713ba7e06f1beb760f20d359c4abed HEAD\x00'
+               'multi_ack thin-pack side-band side-band-64k ofs-delta '
+               'shallow no-progress include-tag multi_ack_detailed no-done\n')
+        path = os.path.join(self.upstream_root, project)
+        repo = git.Repo(path)
+        for ref in repo.refs:
+            r = ref.object.hexsha + ' ' + ref.path + '\n'
+            ret += '%04x%s' % (len(r) + 4, r)
+        ret += '0000'
+        return ret
+
     def getGitUrl(self, project):
         return os.path.join(self.upstream_root, project.name)
 
@@ -494,28 +505,6 @@ class BuildHistory(object):
     def __repr__(self):
         return ("<Completed build, result: %s name: %s #%s changes: %s>" %
                 (self.result, self.name, self.number, self.changes))
-
-
-class FakeURLOpener(object):
-    def __init__(self, upstream_root, url):
-        self.upstream_root = upstream_root
-        self.url = url
-
-    def read(self):
-        res = urllib.parse.urlparse(self.url)
-        path = res.path
-        project = '/'.join(path.split('/')[2:-2])
-        ret = '001e# service=git-upload-pack\n'
-        ret += ('000000a31270149696713ba7e06f1beb760f20d359c4abed HEAD\x00'
-                'multi_ack thin-pack side-band side-band-64k ofs-delta '
-                'shallow no-progress include-tag multi_ack_detailed no-done\n')
-        path = os.path.join(self.upstream_root, project)
-        repo = git.Repo(path)
-        for ref in repo.refs:
-            r = ref.object.hexsha + ' ' + ref.path + '\n'
-            ret += '%04x%s' % (len(r) + 4, r)
-        ret += '0000'
-        return ret
 
 
 class FakeStatsd(threading.Thread):
@@ -1041,14 +1030,6 @@ class ZuulTestCase(BaseTestCase):
 
         self.configure_connections()
         self.sched.registerConnections(self.connections)
-
-        def URLOpenerFactory(*args, **kw):
-            if isinstance(args[0], urllib.request.Request):
-                return old_urlopen(*args, **kw)
-            return FakeURLOpener(self.upstream_root, *args, **kw)
-
-        old_urlopen = urllib.request.urlopen
-        urllib.request.urlopen = URLOpenerFactory
 
         self.merge_server = zuul.merger.server.MergeServer(self.config,
                                                            self.connections)